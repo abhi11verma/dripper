269de52ed9f0a8bd4a7255d5fb140cc406165c21
Abhishek Verma <abhiverma.abhishek@gmail.com>
2017-12-17 21:11:17 +0530

    database connected

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required, login_user, \
+    logout_user, current_user, UserMixin
 import datetime
 
 db = SQLAlchemy()
 
 """ DB Models """
-class User(db.Model):
-    __tablename__ = "users"
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
     userid = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=True)
